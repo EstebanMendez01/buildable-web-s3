f4b10af352d4dda44f89b6c45c03b261c66049f3
Esteban Mendez <mendez01@allegheny.edu>
2023-06-22 12:04:40 -0400

    update

---
+++ src/Configuration.js
@@ -0,0 +1,10 @@
+const awsExports = {
+    Auth: {
+      region: 'us-east-1',
+      userPoolId: 'us-east-1_bahZ8ORG8',
+      userPoolWebClientId: '4q53pcs279lpkbjslc35eo48jb'
+    }
+  };
+  
+  export default awsExports;
+  
